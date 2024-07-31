33e1045cae01184833eae0807d3d0a919bc46b15
Yash Gurjar <ys.gurjar@gmail.com>
2024-07-31 20:23:35 +0000

    feat: add non programming text to sale predictor streamlit page

--- app_pages/page_sale_price_prediction.py
+++ app_pages/page_sale_price_prediction.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 def page_sale_price_prediction():
     """
@@ -9,3 +10,16 @@ def page_sale_price_prediction():
     st.subheader("Estimated sale price for client's inherited houses")
 
     st.write("* Find below 4 inherited houses profile and their estimated sale price")
+
+    df=pd.DataFrame({'Hey':[1,2,3]})
+    
+    st.write(df)
+
+    st.write(f'The sum of predicted sale price is {df.sum(axis=1)}')
+
+    st.subheader("Predicted house sales prices in Ames, Iowa")
+
+
+    st.button("Run Predictive Analysis")
+
+    st.info(f'The predicted sale price for this house in Iowa is')
