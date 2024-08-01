1ec2f06bf0b6fc9c6573dacf23be42ff496f86f9
Yash Gurjar <ys.gurjar@gmail.com>
2024-08-01 05:51:02 +0000

    feat: add data input widgets

--- app_pages/page_sale_price_prediction.py
+++ app_pages/page_sale_price_prediction.py
@@ -46,106 +46,139 @@ def page_sale_price_prediction():
 
     # Fit and transform to predict
     ml_pipeline.fit(X_train, y_train)
-    
+
     # load raw dataset
-    X_inherited_house_data=pd.read_csv("outputs/datasets/collection/inherited_houses.csv")
+    X_inherited_house_data = pd.read_csv(
+        "outputs/datasets/collection/inherited_houses.csv"
+    )
 
     # predict house prices
-    y_predicted_price=ml_pipeline.predict(X_inherited_house_data)
+    y_predicted_price = ml_pipeline.predict(X_inherited_house_data)
 
     # ==== End of core ML logic ====
 
     # append the values
-    X_inherited_house_data['Predicted Sale Price']=y_predicted_price
-    
+    X_inherited_house_data["Predicted Sale Price"] = y_predicted_price
+
     # convert data to int, as float precision is not relevant
-    for col in X_inherited_house_data.select_dtypes(include=['float', 'int']).columns:
+    for col in X_inherited_house_data.select_dtypes(include=["float", "int"]).columns:
         X_inherited_house_data[col] = X_inherited_house_data[col].astype(int)
 
     # render dataframe on page
     st.dataframe(X_inherited_house_data)
 
-    st.write(f"The sum of predicted sale price is {X_inherited_house_data['Predicted Sale Price'].sum()} USD")
+    st.write(
+        f"The sum of predicted sale price is {X_inherited_house_data['Predicted Sale Price'].sum()} USD"
+    )
 
-    st.subheader("Predict house sales prices in Ames, Iowa")
+    st.header("Predict house sales prices in Ames, Iowa")
 
-    # draw input widgets
-    def DrawInputsWidgets():
+    st.write("Let's use our model to predict the price.")
+    st.write("Note that we have only included 4 parameters that affects the model \
+        performance the most as an input")
+    # == Core logic for live predictions ===
 
-    # load dataset
-        df = load_housing_data()
-        df['TotalSF'] = df['TotalBsmtSF'] + df['1stFlrSF'] + df['2ndFlrSF']
+    def run_live_predictions(grLivArea,totalBsmtSF,garageArea,overallQual):
+        
+        # create an empty dataframe with hold live values
+        df = X_train[0:0].copy()
+        
+        df.loc[0,'GrLivArea']=grLivArea
+        df.loc[0,'TotalBsmtSF']=totalBsmtSF
+        df.loc[0,'GarageArea']=garageArea
+        df.loc[0,'OverallQual']= overallQual
 
-        percentageMin, percentageMax = 0.4, 2.0
+        live_predict_y=ml_pipeline.predict(df)
 
-        # we create input widgets only for 6 features	
-        col1, col2, col3, col4 = st.columns(4)
-        col5, col6, col7, col8 = st.columns(4)
+        st.success(f'{live_predict_y}')
 
-        # We are using these features to feed the ML pipeline - values copied from check_variables_for_UI() result
-            
 
-        # create an empty DataFrame, which will be the live data
-        X_live = pd.DataFrame([], index=[0]) 
 
-        # from here on we draw the widget based on the variable type (numerical or categorical)
-        # and set initial values
+    with st.form(key="input data"):
+        col1, col2, col3 = st.beta_columns(3)
+
         with col1:
-            feature = "GarageArea"
-            st_widget = st.number_input(
-                label= feature,
-                min_value= int(df[feature].min()*percentageMin), 
-                max_value= int(df[feature].max()*percentageMax),
-                value= int(df[feature].median()), 
-                step= 50
-                )
-            
-        X_live[feature] = st_widget
-
-
+            grLivArea=st.number_input("Above Ground Living Area (SqFt)",key="GrLivArea", min_value=0, max_value=6000)
         with col2:
-            feature = "GrLivArea"
-            st_widget = st.number_input(
-                label= feature,
-                min_value= int(df[feature].min()*percentageMin), 
-                max_value= int(df[feature].max()*percentageMax),
-                value= int(df[feature].median()), 
-                step= 50
-                )
-        X_live[feature] = st_widget
+            totalBsmtSF=st.number_input("Total Basement Area (SqFt)", key="TotalBsmtSF", min_value=0, max_value=6000)
 
         with col3:
-            feature = "OverallQual"
-            st_widget = st.number_input(
-                label= feature,
-                min_value= 1, 
-                max_value= 10,
-                value= 5, 
-                step=1
-                )
-        X_live[feature] = st_widget
+            garageArea=st.number_input("Garage Area (SqFt)",key="GarageArea", min_value=0, max_value=6000)
+        
+        overallQual=st.select_slider("Overall Quality", options= [1,2,3,4,5,6,7,8,9,10])
+        
+        # form submit button
+        submitted=st.form_submit_button("Run predictions")
 
-        with col4:
-            feature = "TotalBsmtSF"
-            st_widget = st.number_input(
-                label= feature,
-                min_value= int(df[feature].min()*percentageMin), 
-                max_value= int(df[feature].max()*percentageMax),
-                value= int(df[feature].median()), 
-                step= 50
-                )
-        X_live[feature] = st_widget
+        # result display button
+        if (submitted):
+            run_live_predictions(grLivArea,totalBsmtSF,garageArea,overallQual)
+    
 
-        with col5:
-            feature = "YearBuilt"
-            st_widget = st.number_input(
-                label= feature,
-                min_value= int(df[feature].min()*percentageMin), 
-                max_value= date.today().year,
-                value= int(df[feature].median()), 
-                step= 1
-                )
-        X_live[feature] = st_widget
 
-        return X_live
-    st.info(f"The predicted sale price for this house in Iowa is")
+    # Initialize the DataFrame in the session state if it doesn't exist
+    if "df" not in st.session_state or st.session_state.reset_df:
+        st.session_state.df = df  # Start with a default value
+        st.session_state.reset_df = False
+
+
+
+
+
+"""
+    # Store the initial value of widgets in session state
+    if "visibility" not in st.session_state:
+        st.session_state.visibility = "visible"
+        st.session_state.disabled = False
+        st.session_state.horizontal = True
+
+
+    # Start a form
+    with st.form("my_form"):
+        # Create a number input widget and store input in a session state or local variable
+        
+        df.loc[0, "OverallQual"] = st.radio(
+                                "Overall Quality",
+                                [10,9,8,7,6,5,4,3,2,1],
+                                index=None)
+
+        col1, col2, col3 = st.beta_columns(3)
+
+        with col1:
+            st.header("A cat")
+            st.image("https://static.streamlit.io/examples/cat.jpg")
+
+        with col2:
+            st.header("A dog")
+            st.image("https://static.streamlit.io/examples/dog.jpg")
+
+        with col3:
+            st.header("An owl")
+            st.image("https://static.streamlit.io/examples/owl.jpg")
+
+        if submitted:
+            # Update the DataFrame with the new input upon submission
+            st.session_state.df.loc[0, "1stFlrSF"] = df.loc[0, "1stFlrSF"]
+
+    # Display the DataFrame
+    st.write("Values in DataFrame:")
+    st.radio("choose an option", "hey")
+    st.dataframe(df)
+
+    col1, col2 = st.columns(2)
+
+with col1:
+    st.checkbox("Disable radio widget", key="disabled")
+    st.checkbox("Orient radio options horizontally", key="horizontal")
+
+with col2:
+    st.radio(
+        "Set label visibility 👇",
+        ["visible", "hidden", "collapsed"],
+        key="visibility",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        horizontal=st.session_state.horizontal,
+    )
+
+"""
